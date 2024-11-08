285e6f36ea7ef18a0cca279e530ae55215af34f8
mariosantos-05 <marioaugustov.s@gmail.com>
2024-11-08 17:19:04 -0300

    Add of rx tx communication

--- Movement/src/main.cpp
+++ Movement/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <DC_Motor.h>
-#include <Stepper.h>
 #include <Servo.h>
 #include <Color_Sensor.h>
 #include <Ultrassonic_Sensor.h>
 #include <IRSensor.h>
 #include <AccelStepper.h>
+#include <SoftwareSerial.h>
+
 
 // Definiçao de portas    (orientação baseada na garra)
 
@@ -85,7 +86,6 @@ const int Back2_trig = 1;
 const int Back2_echo = 1;
 Sensor L4(Back2_trig, Back2_echo);
 
-
 // Infra 
 
 const int infra_Right = 1;
@@ -104,37 +104,201 @@ const int S2 = 1;
 const int S3 = 1;
 const int Out = 1;
 
-ColorSensor color_front(S0,S1,S2,S3,Out);
+ColorSensor color_Clawn(S0,S1,S2,S3,Out);
+
+const int SF0 = 1;
+const int SF1 = 1;
+const int SF2 = 1;
+const int SF3 = 1;
+const int OutF = 1;
+
+ColorSensor color_front(SF0,SF1,SF2,SF3,OutF);
 
 
+// Global Variables
+SoftwareSerial raspy(10, 11);
+String receivedData = ""; // Armazena a mensagem recebida
+
+const int QUARTER_TURN  = 1000;
+const int HALF_TURN = 10000;
+const int FULL_TURN = 100000;
+const int RUN_TIME = 1000;
 
 void setup() {
   claw.attach(Servo1);
-  Serial.begin(9600);
-
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  Serial.begin(115200); // Inicializa a comunicação serial
+  raspy.begin(115200);
   digitalWrite(12, OUTPUT);
   Stepper.setMaxSpeed(1000);
   Stepper.setAcceleration(500);
+}
+
+
+
+
+void table_high(int hight){
+  if(hight == 5){
+    Stepper.moveTo(200);
+    Stepper.runToPosition();
+    delay(1000);
+  }
+  else if(hight == 10){
+    Stepper.moveTo(200);
+    Stepper.runToPosition();
+    delay(1000);
+  }
+  
+  else{
+    Stepper.moveTo(200);
+    Stepper.runToPosition();
+    delay(1000);    
+  }
+}
+
+void follow_line(int entrance){
+  int flag = 0;
+  int L = IR_L.isLineDetected();
+  int C = IR_C.isLineDetected();
+  int R = IR_R.isLineDetected();
+  if((L == 0 and C == 1) and R == 0){
+    // just go forward
+    LM.move_straight(100);
+    RM.move_straight(100);
+    delay(1000);
+  }
+  else if((L == 1 and C == 0) and R == 0){
+    // right motor go a little
+    FM.move_straight(100);
+    BM.move_straight(100);
+    delay(1000);
+  }
+  else if((L == 0 and C == 0) and R == 1){
+    // Left motor go a little
+    FM.turn_on_motor(REVERSE, 100);
+    BM.turn_on_motor(REVERSE, 100);
+    delay(1000);
+  }
+  else if((L == 1 and C == 1) and R == 0){
+    flag++;
+  }
+  else if((L == 0 and C == 1) and R == 1){
+    flag++;
+  }
+  else if(entrance == 1){
+    //turn 90 to left
+    if((L == 1 and C == 1) and R == 0){
+      FM.turn_on_motor(FORWARD, 100);
+      BM.turn_on_motor(REVERSE, 100);
+      RM.turn_on_motor(FORWARD, 100);
+      LM.turn_on_motor(REVERSE, 100);
+      delay(QUARTER_TURN);
+    }
+  }
+  else if(entrance == 2 and flag == 1){
+    if((L == 1 and C == 1) and R == 0){
+      FM.turn_on_motor(FORWARD, 100);
+      BM.turn_on_motor(REVERSE, 100);
+      RM.turn_on_motor(FORWARD, 100);
+      LM.turn_on_motor(REVERSE, 100);
+      delay(QUARTER_TURN);
+    }
+  }
+}
+
+
+String Rasp_Data(){
+   // Verifica se há dados disponíveis para leitura
+    if (raspy.available() > 0) {
+        char incomingChar = raspy.read(); // Lê o caractere recebido
+
+        // Armazena o caractere na string até encontrar '\n'
+        if (incomingChar != '\n') {
+            receivedData += incomingChar;
+        } else {
+            // Verifica a mensagem completa recebida
+            if (receivedData == "V") {
+                return receivedData;
+            }
+            else {
+                // Converte o ID da tag recebida para um número inteiro
+                int tagId = receivedData.toInt();
+                if (tagId > 0) { // Verifica se foi recebido um ID válido
+                    Serial.print("AprilTag detectada com ID: ");
+                    Serial.println(tagId);
+                    // Aqui você pode adicionar lógica específica para cada ID
+                    return receivedData;
+                }
+            }
+            // Limpa a string para receber a próxima mensagem
+            receivedData = "";
+        }
+    }
+}
+
+void service_table(){
 
 }
 
+void precision_table(){
+
+}
+
+void container_table(){
+
+}
+
+void shelve(){
+
+}
+
+void Start(){
+  while (F1.getDistance()> 10){
+    RM.turn_on_motor(FORWARD, 100);
+    LM.turn_on_motor(FORWARD, 100);
+  }
+  service_table(); 
+  while (!IR_C.isLineDetected()){
+    BM.turn_on_motor(FORWARD, 100);
+    FM.turn_on_motor(FORWARD, 100);  
+  }
+  // 180 turn
+  BM.turn_on_motor(FORWARD, 100);
+  FM.turn_on_motor(REVERSE, 100);
+  LM.turn_on_motor(FORWARD, 100);
+  RM.turn_on_motor(REVERSE, 100);
+  delay(10000);
+}
+
+void Finish(bool shelve){
+  int L = IR_L.isLineDetected();
+  int R = IR_R.isLineDetected();
+  if (shelve){
+    if(L){
+      while (F1.getDistance()> 10){
+        RM.turn_on_motor(FORWARD, 100);
+        LM.turn_on_motor(FORWARD, 100);
+      }
+    }
+  }
+  else{
+    if(R){
+      FM.turn_on_motor(FORWARD, 100);
+      BM.turn_on_motor(REVERSE, 100);
+      RM.turn_on_motor(FORWARD, 100);
+      LM.turn_on_motor(REVERSE, 100);
+      delay(QUARTER_TURN);
+    }
+  }
+}
+
+
 void loop() {
-  Stepper.moveTo(200);
-  Stepper.runToPosition();
-  delay(1000);
-  Stepper.moveTo(-200);
-  Stepper.runToPosition();
-  delay(1000);
-  
+
+  Start();
+  int bifurcation = 1; 
+  follow_line(bifurcation);
+  //  Insert here the desired table
+  bool shelve = false;
+  Finish(shelve);
+
 }
